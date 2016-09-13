85b435296766fa0b5c1a641de43fe22ec5819b7b
Lincoln Kupke <lincolnkupke@gmail.com>
2016-09-13 14:07:50 -0400

    added styling for menubar and for mobile navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email,
+   presence: true,
+   allow_nil: false,
+   format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i },
+   uniqueness: true
+
+  def admin?
+    role == "admin"
+  end
+
+  def fullname
+    "#{first_name} #{last_name}"
+  end
 end
