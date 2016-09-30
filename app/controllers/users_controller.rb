8775607ab6d8098eb307787829e8114f3399ffd1
Lincoln Kupke <lincolnkupke@gmail.com>
2016-09-30 12:18:28 -0400

    added a route authorize checker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
-
+    if params["id"] != current_user.id.to_s
+      flash[:error] = "Sorry you do not have access to that profile!"
+      redirect_to "root"
+    end
+    @user_info = current_user
   end
 end
