eca5db28ebd17ca8128778aa1a68638052d6a01a
Lincoln Kupke <lincolnkupke@gmail.com>
2016-10-07 07:13:38 -0400

    added axios to package json file

--- react/src/main.js
+++ react/src/main.js
@@ -1,6 +1,10 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import promise from 'redux-promise';
 
 $(function() {
   ReactDOM.render(
