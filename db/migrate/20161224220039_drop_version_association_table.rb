a00e69aff8836fdcff265b7be9a2cdee3d637aa9
Lincoln Kupke <lincolnkupke@gmail.com>
2017-01-05 11:01:12 -0500

    possible fix for db

--- db/migrate/20161224220039_drop_version_association_table.rb
+++
@@ -1,8 +0,0 @@
-class DropVersionAssociationTable < ActiveRecord::Migration
-  def up
-    drop_table :version_associations
-  end
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20161224220357_drop_version_association_table2.rb
+++
@@ -1,8 +0,0 @@
-class DropVersionAssociationTable2 < ActiveRecord::Migration
-  def up
-    drop_table :version_associations
-  end
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
